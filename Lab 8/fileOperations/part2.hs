a1b092e7ea164a221978560d58bdeabc2d0c9f74
hintona <hinton.alex18@gmail.com>
2022-04-11 13:25:22 -0400

    done

--- Lab 8/fileOperations/part2.hs
+++ Lab 8/fileOperations/part2.hs
@@ -1,13 +1,22 @@
 import System.IO
 
-addl [x:xs]
+addl xs = 
+    if length xs > 1
+        let num = (last xs) + (last (init xs))
+        ys <- num
+        ys ++ addl (init xs)
+        return ys
+    else 
+        return (init xs)
+
+
 
 main = do
     contents <- readFile "../dataset/numbers.dat"
     let input = lines contents
     let procInput = interpret input
     
-    let fin = procInput
+    let fin = addl procInput
     print fin
 
     let output = map show fin
